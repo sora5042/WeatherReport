a5779bf905fb15c1c58417efac734d007813b249
sora5042 <esuora5042@gmail.com>
2025-04-14 15:31:26 +0900

    Introduce SwiftFormat

--- WeatherReportTests/Sources/WeatherReportTests.swift
+++ WeatherReportTests/Sources/WeatherReportTests.swift
@@ -9,9 +9,7 @@ import Testing
 @testable import WeatherReport
 
 struct WeatherReportTests {
-
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
-
 }
--- WeatherReportUITests/WeatherReportUITestsLaunchTests.swift
+++ WeatherReportUITests/WeatherReportUITestsLaunchTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 final class WeatherReportUITestsLaunchTests: XCTestCase {
-
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
